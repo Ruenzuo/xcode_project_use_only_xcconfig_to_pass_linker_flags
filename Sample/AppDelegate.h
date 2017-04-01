9f638bd4af0855ab5bbcf2f4c8248c40dc3b280f
Renzo Crisóstomo <ruenzuo@gmail.com>
2017-04-01 23:18:20 +0200

    Initial commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Renzo Crisóstomo on 01.04.17.
+//  Copyright © 2017 Renzo Crisóstomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
